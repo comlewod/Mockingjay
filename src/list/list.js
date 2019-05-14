922ff72817f8a9970f970398de11f518898b85f0
xgquan <guangquanxie@shein.com>
2019-05-14 20:40:33 +0800

    add json

--- src/list/list.js
+++ src/list/list.js
@@ -2,11 +2,38 @@ export default {
 	data(){
 		return {
 			input: '',
+
+			jsonStr: '',
+			jsonErr: false,
+			jsonArr: [],
 		}
 	},
 	methods: {
 		addRouter(){
-			console.log(123)
+		},
+		blurJson(){
+			let str = this.jsonStr.trim()
+			if( str ){
+				let obj = null
+				try {
+					obj = JSON.parse(str)
+				} catch(e) {
+					this.jsonErr = true
+					return
+				}
+				console.log(obj)
+				this.objectLoop(obj)	
+			}
+		},
+		objectLoop(obj){
+			for( let key in obj ){
+				let value = obj[value]
+				let item = {
+					key,
+					value,
+				}
+				this.jsonArr.push(item)
+			}
 		}
 	},
 	components: {
--- src/list/list.vue
+++ src/list/list.vue
@@ -5,6 +5,24 @@
 			<el-input v-model="input" placeholder="请输入内容"></el-input>
 			<el-button type="primary" @click="addRouter">添加路由</el-button>
 		</div>
+		<div>
+			<el-input
+				type="textarea"
+				:rows="10"
+				placeholder="请输入json字符串"
+				@focus="jsonErr = false"
+				@blur="blurJson"
+				v-model="jsonStr"
+			>
+			</el-input>
+			<el-alert
+				v-show="jsonErr"
+				title="json格式错误"
+				type="error"
+				:closable="false"
+			>
+			</el-alert>
+		</div>
 	</div>
 </template>
 
